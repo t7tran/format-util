ff4891031ec95e3ebeaa9ec59d69a75fb748f57a
Tien Tran <tien@transfamily.info>
2021-02-18 13:24:23 +1100

    Support %o with stringifying superfluous argument objects

--- format.js
+++ format.js
@@ -1,37 +1,42 @@
+function tostring(arg) {
+  return typeof arg == 'object' ? JSON.stringify(arg) : arg;
+}
+
 function format(fmt) {
-  var re = /(%?)(%([jds]))/g
-    , args = Array.prototype.slice.call(arguments, 1);
-  if(args.length) {
-    fmt = fmt.replace(re, function(match, escaped, ptn, flag) {
-      var arg = args.shift();
-      switch(flag) {
-        case 's':
-          arg = '' + arg;
-          break;
-        case 'd':
-          arg = Number(arg);
-          break;
-        case 'j':
-          arg = JSON.stringify(arg);
-          break;
-      }
-      if(!escaped) {
-        return arg; 
-      }
-      args.unshift(arg);
-      return match;
-    })
-  }
+var re = /(%?)(%([ojds]))/g
+  , args = Array.prototype.slice.call(arguments, 1);
+if(args.length) {
+  fmt = fmt.replace(re, function(match, escaped, ptn, flag) {
+    var arg = args.shift();
+    switch(flag) {
+      case 's':
+        arg = '' + arg;
+        break;
+      case 'd':
+        arg = Number(arg);
+        break;
+      case 'o':
+      case 'j':
+        arg = tostring(arg);
+        break;
+    }
+    if(!escaped) {
+      return arg;
+    }
+    args.unshift(arg);
+    return match;
+  })
+}
 
-  // arguments remain after formatting
-  if(args.length) {
-    fmt += ' ' + args.join(' ');
-  }
+// arguments remain after formatting
+if(args.length) {
+  fmt += ' ' + args.map(tostring).join(' ');
+}
 
-  // update escaped %% values
-  fmt = fmt.replace(/%{2,2}/g, '%');
+// update escaped %% values
+fmt = fmt.replace(/%{2,2}/g, '%');
 
-  return '' + fmt;
+return '' + fmt;
 }
 
 module.exports = format;
